72baff9f47816b8824f59a719ed36558cc0e52c2
1shaked <shaked1hen@gmail.com>
2023-02-08 20:58:53 +0200

    new code uploaded

---
+++ 7ass/js/index.js
@@ -0,0 +1,30 @@
+
+let username  = "Dax"
+let last_name = 'chen'
+
+let age = 24
+
+console.log("Hey " + username + " by " + last_name + " " +age);
+// console.log(age);
+
+
+// line comment
+/*
+
+multi line comment
+*/
+
+
+
+let isStudent = false; // true
+console.log(isStudent);
+isStudent = true;
+console.log(isStudent);
+
+// if else statements 
+if (isStudent) {
+    console.log('I am student');
+} 
+else {
+    console.log('WOW you are not a student');
+}
