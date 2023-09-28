504c7d17cf814f836f57a2a830e02409590a13fd
1shaked <40497915+1shaked@users.noreply.github.com>
2023-09-28 20:36:02 +0300

    questions

---
+++ 8ass/js/index.js
@@ -0,0 +1,88 @@
+// create a script that ask the user his age and console.log his age times 5
+
+// create a script that ask the user his first name, and then his last name and print his full name to the screen
+
+
+// create a script that calculate the sum of all numbers from one to 1000 and prints it 
+
+// create a script that calculate the sum of all even numbers from one to 1000 and prints it 
+
+// create a script that take the array let arr = [1,2,3] reverse it and print it to the screen
+// example [1,2,3] -> [3,2,1]
+
+// create a script that calculate the multiplication of all the odd numbers between 5 and 25
+
+// create a script that calculate the multiplication of the sum of all the even numbers between 1 and 100 and sum of all the odd number between 101 and 201 
+
+
+// create a script that calculate the sum of let arr = [100, 200 , 300 , 567]
+
+// create a script that calculate the multiplication of let arr = [100, 200 , 300 , 567]
+
+// create a script that calculate the multiplication of let arr = [100, 200 , 300 , 567]
+
+// create a script that take the let obj = {age: 10 , v: 20, v2: 55} and calculate the sum of all the values
+
+// create a script that script that take an object and calculate the multiplication of all the values if and only if the key length is less then 3
+// example let obj = {ages: 10 , v: 20, v2: 55} -> 75
+
+// create a function that take a number and return it +2
+
+// create a function that take a number and return it *2
+
+// create a function that take an array and return the sum of it's elements 
+
+// create a function that take an array and return the multiplication of it's elements
+
+// create a function that take an object and return the sum of all the values
+
+// create a function that take an object and return the multiplication of all the values
+
+// create a function that take two array and return multiplication of the sum of the two arrays
+// example arr = [1 , 2 , 3] , arr2 = [4 , 5, 1] -> 70
+
+// create a function that take two array and return sum of the multiplication of the two arrays
+// example arr = [1 , 2 , 3] , arr2 = [4 , 5, 1] -> 35
+
+// create a function that take an array and return the sum of all the even numbers
+
+// create a function that take an array and return the multiplication of all the odd numbers
+
+// create a function that take an array and return the sum of all the even numbers
+
+// create a function that take an array and return the multiplication of all the odd numbers
+
+// create a function that take an array and return the sum of all the even numbers
+
+// create a function that take an array and return the multiplication of all the odd numbers
+
+// create a function that take an array and return the sum of all the even numbers
+
+// create a function that take an array and return the multiplication of all the odd numbers
+
+// create a function that take an array and return the sum of all the even numbers
+
+// create a function that take an array and return the multiplication of all the odd numbers
+
+// create a function that take an array and return the sum of all the even numbers
+
+// create a function that take an array and return a new array where each element is multiplied by 2
+
+// create a function that take an array and return a new array where each elements are reversed
+// example [1,2,3] -> [3,2,1]
+
+// create a function that take an array and return a new array where each elements are reversed and multiplied by 2
+// example [1,2,3] -> [6,4,2]
+
+
+// create a recursive function that take a number and return the sum of all the numbers from 1 to that number
+// example 5 -> 1+2+3+4+5 = 15
+
+// create a recursive function that take a number and return the multiplication of all the numbers from 1 to that number
+// example 5 -> 1*2*3*4*5 = 120
+
+// create a recursive function that take a number and return the sum of all the even numbers from 1 to that number
+// example 5 -> 2+4 = 6
+
+// create a recursive function that take a number and return the multiplication of all the odd numbers from 1 to that number
+// example 5 -> 1*3*5 = 15
