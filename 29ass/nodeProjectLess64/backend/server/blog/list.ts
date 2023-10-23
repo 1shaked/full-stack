ea11a0cb8a4c78e71e762a0201e9a15f8a3d9366
1shaked <shaked1hen@gmail.com>
2023-10-23 19:01:02 +0300

    added query and params

--- 29ass/nodeProjectLess64/backend/server/blog/list.ts
+++ 29ass/nodeProjectLess64/backend/server/blog/list.ts
@@ -3,7 +3,7 @@ import { prisma } from "../connection";
 
 
 export async function ListRouterHandler(req: Request, res: Response) {
-    const blogs = await prisma.blog.findMany()
+    const blogs = await prisma.blog.findMany(); // SELECT * FROM blog
     res.send({
         message: 'This is a list router handler',
         blogs
