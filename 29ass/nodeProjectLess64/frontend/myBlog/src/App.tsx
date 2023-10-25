4527cb83ec24b1f915988937fe8281c533be8cbf
1shaked <40497915+1shaked@users.noreply.github.com>
2023-10-25 20:38:01 +0300

    done

--- 29ass/nodeProjectLess64/frontend/myBlog/src/App.tsx
+++ 29ass/nodeProjectLess64/frontend/myBlog/src/App.tsx
@@ -4,10 +4,14 @@ import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import { AddForm } from "./components/add_form/add_form";
 import { GetDataAndAddForm } from "./components/get_data_and_add_form";
+import { SelectMuiExample } from "./components/mui_examples/select";
+import { BasicTableMuiExample } from "./components/mui_examples/table_mui";
 
 const queyClient = new QueryClient();
 function App() {
   return (<QueryClientProvider client={queyClient} >
+    <SelectMuiExample />
+    <BasicTableMuiExample />
     {/* <GetData />
     <AddForm /> */}
     <GetDataAndAddForm />
---
+++ python/ex1.py
@@ -0,0 +1,19 @@
+print('wow')
+
+x = 15
+print(x)
+
+print(x * 10)
+
+x = [10  , 15 , 2 , 6]
+
+for item in x: # for of
+    print(item)
+
+# x = {
+#     'name': 'shaked',
+#     'last': 'chen',
+#     'age': 20
+# }
+
+# print(x['age'])
