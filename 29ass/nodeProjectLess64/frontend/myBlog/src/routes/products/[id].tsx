b4b544fbe371bcae91a289cca838abe994738544
1shaked <shaked1hen@gmail.com>
2023-10-30 19:56:49 +0200

    how to use params

--- 29ass/nodeProjectLess64/frontend/myBlog/src/routes/products/[id].tsx
+++ 29ass/nodeProjectLess64/frontend/myBlog/src/routes/products/[id].tsx
@@ -1,7 +1,9 @@
+import { useParams, useSearchParams } from "react-router-dom"
 
 
 export function ProductIdPage() {
+    const params = useParams();
     return <h1>
-        ProductIdPage
+        ProductIdPage {JSON.stringify(params)}
     </h1>
 }
