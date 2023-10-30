2a9132d9c926a36881b0b6fc1942ec85b8c1dfe7
1shaked <shaked1hen@gmail.com>
2023-10-30 20:22:38 +0200

    out let react layout

--- 29ass/nodeProjectLess64/frontend/myBlog/src/routes/index.tsx
+++ 29ass/nodeProjectLess64/frontend/myBlog/src/routes/index.tsx
@@ -2,12 +2,17 @@ import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { HomePage } from "./home";
 import { MainProducts } from "./products/main";
 import { ProductIdPage } from "./products/[id]";
+import { LayoutStory } from "./story/layout";
 
 export function RouterReact() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/products">
+        <Route path=":id" element={<ProductIdPage />} errorElement={<h1>error!</h1>} />
+        <Route index element={<MainProducts />} />
+      </Route>
+      <Route path="/story" element={<LayoutStory />}>
         <Route path=":id" element={<ProductIdPage />} errorElement={<h1>error!</h1>} />
         <Route index element={<MainProducts />} />
       </Route>
