6dc8bcfccbce19d61b9b4e95b8c6975606ea53cb
1shaked <shaked1hen@gmail.com>
2023-11-01 18:47:22 +0200

    basic signal

--- 29ass/nodeProjectLess64/frontend/myBlog/src/routes/signals/class/page.tsx
+++ 29ass/nodeProjectLess64/frontend/myBlog/src/routes/signals/class/page.tsx
@@ -1,7 +1,19 @@
+import { useSignal } from "@preact/signals-react"
 
 
 export function SignalsIntroClass() {
+    const count = useSignal(0);
+    
     return <main>
         <h1>SignalsIntroClass</h1>
+        <button onClick={() => {
+            count.value = count.value - 1;
+        }}>-</button>
+        <p>
+            {count.value}
+        </p>
+        <button onClick={() => {
+            count.value = count.value + 1;
+        }}>+</button>
     </main>
 }
