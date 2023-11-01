06ace86bf2678ae6ef1f7039456c8692cce5b6d1
1shaked <shaked1hen@gmail.com>
2023-11-01 19:06:08 +0200

    signal outside

--- 29ass/nodeProjectLess64/frontend/myBlog/src/components/signalsClass/introSignals.tsx
+++ 29ass/nodeProjectLess64/frontend/myBlog/src/components/signalsClass/introSignals.tsx
@@ -1,7 +1,27 @@
+import { signal } from "@preact/signals-react"
+import { useState } from "react";
 
+interface TodoInterface {
+    text: string;
+}
+
+const todo = signal<TodoInterface[]>([
+    { text: 'x'},
+    { text: 'y'},
+    { text: 'z'},
+]);
 
 export function IntroSignals() {
+    const [todoText, setTodoText] = useState('');
     return <div>
+        <div>
+            {todoText}
+            <input type="text" onChange={(e) => setTodoText(e.target.value)} />
+            <button onClick={() => {
+                todo.value = [...todo.value, { text: todoText }]
+            }}>add todo</button>
+        </div>
         signals
+        {todo.value.map((todo, index) => <p key={index}> {todo.text} </p>)}
     </div>
 }
--- 29ass/nodeProjectLess64/frontend/myBlog/src/routes/signals/class/page.tsx
+++ 29ass/nodeProjectLess64/frontend/myBlog/src/routes/signals/class/page.tsx
@@ -1,12 +1,13 @@
 import { computed, effect, useSignal } from "@preact/signals-react"
+import { IntroSignals } from "../../../components/signalsClass/introSignals";
 
 
 export function SignalsIntroClass() {
     const count = useSignal(0);
     const doubleNumber = computed(() => count.value  * 2);
-    effect(() => {
-        console.log(count.value)
-    })   
+    // effect(() => {
+    //     console.log(count.value)
+    // })   
     return <main>
         <h1>SignalsIntroClass</h1>
         <button onClick={() => {
@@ -21,5 +22,6 @@ export function SignalsIntroClass() {
         <p>
             {doubleNumber.value}
         </p>
+        <IntroSignals />
     </main>
 }
