46b93b7f0e1333f2ea89670aaf9c017371c8dc29
1shaked <shaked1hen@gmail.com>
2023-11-02 19:33:01 +0200

    arr ex 1

--- python/arr_ex1.py
+++ python/arr_ex1.py
@@ -13,17 +13,19 @@ example [2 ,  3, 4 ] -> 24
 
 def MultiArray(array: list[int]):
     count_multi = 1
-    for num in array:
-        count_multi = count_multi * num
+    for num_in_arr in array:
+        count_multi = count_multi * num_in_arr
 
     return count_multi
 
 
 arr1 = [1 , 2 , 3]
-res_arr1 = MultiArray(arr1)
-print(f'example {arr1} - {res_arr1}')
+res_arr1 = MultiArray(array = arr1)
+# print(arr1)
+# print(res_arr1)
+print(f'{arr1} ---> {res_arr1}')
 
 
 arr2 = [1 , 2 , 3]
-res_arr2 = MultiArray(arr2)
-print(f'example {arr2} - {res_arr2}')
+res_arr2 = MultiArray(array = arr2)
+print(f'{arr2} ---> {res_arr2}')
