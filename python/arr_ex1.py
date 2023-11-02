87ec59bfb81b866f5e6272dd7aa6ffbfaf8505d6
1shaked <shaked1hen@gmail.com>
2023-11-02 19:24:06 +0200

    string and sol

--- python/arr_ex1.py
+++ python/arr_ex1.py
@@ -10,3 +10,20 @@ example [2 ,  2, 4 ] -> 16
 example [2 ,  3, 4 ] -> 24
 
 '''
+
+def MultiArray(array: list[int]):
+    count_multi = 1
+    for num in array:
+        count_multi = count_multi * num
+
+    return count_multi
+
+
+arr1 = [1 , 2 , 3]
+res_arr1 = MultiArray(arr1)
+print(f'example {arr1} - {res_arr1}')
+
+
+arr2 = [1 , 2 , 3]
+res_arr2 = MultiArray(arr2)
+print(f'example {arr2} - {res_arr2}')
