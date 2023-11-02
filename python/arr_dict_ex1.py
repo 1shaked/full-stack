ee1ebb6503865b1b462b8e78e4a5d960457ea08e
1shaked <shaked1hen@gmail.com>
2023-11-02 18:59:22 +0200

    arr dict ex 1

---
+++ python/arr_dict_ex1.py
@@ -0,0 +1,23 @@
+'''
+write a function that take an array of map of shape
+{ 'name': string, 'lastName': string, age: number}
+
+and calculate the sum of the letters + age
+
+example [ {name: 'j', age:1, lastName: 'x'} ] -> 3
+
+example [ {name: 'jj', age:1, lastName: 'x'} ] -> 4
+
+example [ {name: 'jj', age:2, lastName: 'x'} ] -> 5
+
+example [ {name: 'jj', age:1, lastName: 'xx'} ] -> 5
+
+example [ {name: 'jj', age:2, lastName: 'xy'} ] -> 6
+
+example [ {name: 'jj', age:2, lastName: 'xyw'} ] -> 7
+
+example [ {name: 'jj', age:3, lastName: 'xyw'} ] -> 8
+
+
+
+'''
