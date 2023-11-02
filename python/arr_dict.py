9c2cba49e9804f74aa648572ecfb691aea3232e3
1shaked <shaked1hen@gmail.com>
2023-11-02 17:51:34 +0200

    arr _ dict

---
+++ python/arr_dict.py
@@ -0,0 +1,10 @@
+
+
+arr_dict = [
+    { 'name': 'y', 'age': 25, 'isSingle': False, }, # 0
+    { 'name': 'x', 'age': 5, 'isSingle': True, },   # 1
+    { 'name': 'T', 'age': 34, 'isSingle': True, },  # 2
+    { 'name': 'L', 'age': 101, 'isSingle': False, },# 3
+]
+
+print(arr_dict[-1]['age'])
