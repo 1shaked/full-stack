1ed3f78216a66ec414ee14e5a3cb58828eabd73a
1shaked <shaked1hen@gmail.com>
2023-11-02 20:36:27 +0200

    done arr dict ex 1

--- python/arr_dict_ex1.py
+++ python/arr_dict_ex1.py
@@ -18,6 +18,23 @@ example [ {name: 'jj', age:2, lastName: 'xyw'} ] -> 7
 
 example [ {name: 'jj', age:3, lastName: 'xyw'} ] -> 8
 
-
+example [ {name: 'jj', age:3, lastName: 'xyw'} , {name: 'j', age:1, lastName: 'x'}] -> 11
 
 '''
+
+def CalculateSumArrObject(array: list[dict[str, any]]): # create the function to calculate the sum
+    count = 0 # create the count for the array, to be used
+    for human_dict in array: # looping on the human dictionary
+        len_name = len(human_dict['name']) # get len of the name 
+        len_last = len(human_dict['lastName']) # get the len last name
+        count = count +  len_name + human_dict['age'] + len_last # calculate the sum for this human  
+
+    return count # return the count to use
+
+
+# create the arr1 to use as example
+arr1 = [ {'name': 'jj', 'age':3, 'lastName': 'xyw'} , {'name': 'j', 'age':1, 'lastName': 'x'}]
+# save the result in a variable res_arr1
+res_arr1 = CalculateSumArrObject(array = arr1)
+# print the result
+print(f'{arr1} --> {res_arr1}')
