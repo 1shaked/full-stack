b6e7f224605e664bc3e0659a965298646897c0b5
1shaked <shaked1hen@gmail.com>
2023-11-02 18:44:55 +0200

    change var name

--- python/functions.py
+++ python/functions.py
@@ -3,11 +3,13 @@ def arrCharLength(arr: list[str]):
     count = 0;
     for word in arr:
         count = count + len(word)
+
+
     return count
 
 
-arr = ['a', 'bc', 'd'] # ->a= 1 , b=2, c=3, d=4
+arr_of_words = ['a', 'bc', 'd'] # -> a= 1 , b=2, c=3, d=4
 
-arr_char_len_of_arr = arrCharLength(arr)
+arr_char_len_of_arr = arrCharLength(arr_of_words)
 
 print(arr_char_len_of_arr)
