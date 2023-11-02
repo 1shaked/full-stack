3498df43914ac24888733adb7b68ed2932210856
1shaked <shaked1hen@gmail.com>
2023-11-02 20:54:02 +0200

    more

--- python/basic_ex1.py
+++ python/basic_ex1.py
@@ -8,12 +8,7 @@ create a variable name age with the value 48 and print his value
 
 '''
 create a variable name  first_name ,last_name with the values 'x' and 'y' respective value
-'''
-
-
-'''
-create a variable name  first_name ,last_name with the values 'x' and 'y' respective value
-as the following 'x -- y'
+print the values as the following 'x -- y'
 '''
 
 '''
@@ -24,4 +19,108 @@ and calculate his sum
 '''
 create a variable name arr with the value [ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8]
 and calculate his multiplication
+'''
+
+'''
+create a variable name obj with the value { name: 'x' , lastName: 'y' } 
+and print his value
+'''
+
+
+'''
+create a variable name obj with the value { name: 'x' , lastName: 'y' } 
+and print his value as the following 'x -- y'
+'''
+
+
+'''
+create a variable name arr with the value [ { name: 'x' , lastName: 'y' } , { name: 'w' , lastName: 'z' } ] 
+and print his value as the following 
+'x -- y'
+'w -- z'
+'''
+
+'''
+create a variable name arr with the value [ 's1' , 's2' , 's3' , 's4' , 's5' , 's6' , 's7' , 's8' ] 
+and print his value as the following 
+'s1'
+'s2'
+'s3'
+'s4'
+'s5'
+'s6'
+'s7'
+'s8'
+'''
+
+'''
+create a while loop that loops over an array and print all the indexes
+example = [1, 2] -> 0 ,1
+example = [3, 4] -> 0 ,1
+example = [3, 4, 5] -> 0 ,1, 2
+example = [3, 8, 5] -> 0 ,1, 2
+example = [3, 4, 5, 10] -> 0 ,1, 2, 3
+'''
+
+'''
+create a while loop that loops over an array and print all the values
+example = [1, 2] -> 1 ,2
+example = [3, 4] -> 3 ,4
+example = [3, 4, 5] -> 3 ,4, 5
+example = [3, 8, 5] -> 3 ,8, 5
+example = [3, 4, 5, 10] -> 3 ,4, 5, 10
+'''
+
+'''
+create a while loop that loops over an array and print all the values and indexes
+example = [1, 2] -> 0 1 ,1 2
+example = [3, 4] -> 0 3 ,1 4
+example = [3, 4, 5] -> 0 3 ,1 4, 2 5
+example = [3, 8, 5] -> 0 3 ,1 8, 2 5
+example = [3, 4, 5, 10] -> 0 3 ,1 4, 2 5, 3 10
+'''
+
+'''
+create a function that take a string and returns his len
+example = 'x' -> 1
+example = 'xy' -> 2
+example = 'xyz' -> 3
+example = 'xyzw' -> 4
+'''
+
+'''
+create a function that take a string and returns his len + 1
+example = 'x' -> 2
+example = 'xy' -> 3
+example = 'xyz' -> 4
+example = 'xyzw' -> 5
+'''
+
+'''
+create a function that take a string and reverse it
+example = 'x' -> 'x'
+example = 'xy' -> 'yx'
+example = 'xyz' -> 'zyx'
+example = 'xyzw' -> 'wzyx'
+'''
+
+'''
+create a function that take a string and count how many spaces there is in the string
+example = 'x' -> 0
+example = 'xy' -> 0
+example = 'x yz' -> 1
+example = 'x y z' -> 2
+example = 'x y z w' -> 3
+'''
+
+'''
+create a function that take two string and count how many time this string apers in the other string
+example = 'x' , 'x' -> 1
+example = 'xy' , 'x' -> 1
+example = 'x yz' , 'x' -> 1
+example = 'x y z' , 'x' -> 1
+example = 'x y x w' , 'x' -> 2
+example = 'x y x x' , 'x' -> 3
+example = 'x x x x' , 'x' -> 4
+example = 'x x x x' , 'x' -> 4
 '''
