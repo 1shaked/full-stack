e62ebd0c90b3a05eeb7e370678550b510f86c063
1shaked <shaked1hen@gmail.com>
2023-11-02 17:21:29 +0200

    new changes

---
+++ python/var.py
@@ -0,0 +1,8 @@
+# string number boolean
+fistName = 'shaked' # string -> str
+age: int = 20 # number (integer) -> int 
+money: float = 10.1 # number (float) -> float
+isMarried: bool = False # boolean -> bool
+
+
+
