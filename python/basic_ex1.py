d851a65de44914aed1759872f45ec882f9110435
1shaked <shaked1hen@gmail.com>
2023-11-02 20:41:06 +0200

    more

--- python/basic_ex1.py
+++ python/basic_ex1.py
@@ -3,5 +3,25 @@ create a variable name first_name with the value 'wow' and print his value
 '''
 
 '''
-create a variable name first_name with the value 'wow' and print his value
+create a variable name age with the value 48 and print his value
+'''
+
+'''
+create a variable name  first_name ,last_name with the values 'x' and 'y' respective value
+'''
+
+
+'''
+create a variable name  first_name ,last_name with the values 'x' and 'y' respective value
+as the following 'x -- y'
+'''
+
+'''
+create a variable name arr with the value [0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8]
+and calculate his sum
+'''
+
+'''
+create a variable name arr with the value [ 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8]
+and calculate his multiplication
 '''
