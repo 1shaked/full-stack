a8fb821378e90b723a2ad2df883e6d744fea587f
1shaked <shaked1hen@gmail.com>
2023-11-02 18:50:58 +0200

    arr =

--- python/functions.py
+++ python/functions.py
@@ -7,10 +7,10 @@ def arrCharLength(arr: list[str]):
     return count
 
 arr_of_words = ['a', 'bc', 'd'] # -> a= 1 , b=2, c=3, d=4
-arr_char_len_of_arr = arrCharLength(arr=arr_of_words)
+arr_char_len_of_arr = arrCharLength(arr = arr_of_words)
 
 arr_words_2 = ['x', 'xy', 'y', 'z']
-arr_words_2_char_length = arrCharLength(arr=arr_words_2)
+arr_words_2_char_length = arrCharLength(arr = arr_words_2)
 
 print(arr_char_len_of_arr)
 print(arr_words_2_char_length)
