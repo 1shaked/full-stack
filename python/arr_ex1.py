cd3ca069f94c7ff7911379b1b5b0279abd1acfa2
1shaked <shaked1hen@gmail.com>
2023-11-02 18:54:23 +0200

    arr ex1

---
+++ python/arr_ex1.py
@@ -0,0 +1,10 @@
+'''
+write a function that take array of numbers and calculates the multiplication of all of them
+
+example [1 ,  2, 3 ] -> 6
+
+example [1 ,  2, 4 ] -> 8
+
+example [2 ,  2, 4 ] -> 16
+
+'''
