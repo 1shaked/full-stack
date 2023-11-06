6e3fa518e0fd775800d75ef5c2e198baf5e809b9
1shaked <shaked1hen@gmail.com>
2023-11-06 17:41:00 +0200

    added the trpc service

---
+++ 29ass/nodeProjectLess64/backend/server/booksTrpc/index.ts
@@ -0,0 +1,10 @@
+import { initTRPC } from '@trpc/server';
+ const t = initTRPC.create();
+const router = t.router;
+const publicProcedure = t.procedure;
+const appRouter = router({
+    // ...
+  });
+
+
+export type AppRouter = typeof appRouter;
--- 29ass/nodeProjectLess64/frontend/myBlog/src/App.tsx
+++ 29ass/nodeProjectLess64/frontend/myBlog/src/App.tsx
@@ -4,17 +4,17 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 
 
-import { BrowserRouter,HashRouter} from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom'
 import { RouterReact } from "./routes";
 const queyClient = new QueryClient();
 function App() {
-  return (<HashRouter>
+  return (<BrowserRouter>
     <QueryClientProvider client={queyClient} >
       <div>
       </div>
       <RouterReact />
     </QueryClientProvider>
-  </HashRouter>
+  </BrowserRouter>
   );
 }
 
