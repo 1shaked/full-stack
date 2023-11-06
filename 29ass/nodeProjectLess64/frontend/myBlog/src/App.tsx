2ef38a60336b91f32558eb8435ae1e8dc9128966
1shaked <shaked1hen@gmail.com>
2023-11-06 17:25:02 +0200

    example use the # ROUTER

--- 29ass/nodeProjectLess64/frontend/myBlog/src/App.tsx
+++ 29ass/nodeProjectLess64/frontend/myBlog/src/App.tsx
@@ -4,17 +4,17 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 
 
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter,HashRouter} from 'react-router-dom'
 import { RouterReact } from "./routes";
 const queyClient = new QueryClient();
 function App() {
-  return (<BrowserRouter>
+  return (<HashRouter>
     <QueryClientProvider client={queyClient} >
       <div>
       </div>
       <RouterReact />
     </QueryClientProvider>
-  </BrowserRouter>
+  </HashRouter>
   );
 }
 
