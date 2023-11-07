594eae35cc7a06a39d2f60f6b7e5226a84252109
1shaked <40497915+1shaked@users.noreply.github.com>
2023-11-07 19:08:31 +0200

    task more

--- python/functions.py
+++ python/functions.py
@@ -8,3 +8,31 @@ example -> [ 2 , 3] -> return 6
 example -> [ 4 ] -> return 4
 '''
 
+'''
+create a function that take an array of array of numbers and calculate the sum
+
+[[ 1, 2 , 3] , [4 , 5 , 6] , [7 , 8]] -> return 36
+[ [4 , 5 , 6] , [7 , 8]] -> return 30
+'''
+
+'''
+create a function that take an array of array of numbers and calculate the multiplication
+
+[ [1,  2], [3] ] -> return 6
+[ [4], [3] ] -> return 12
+[ [4], [3,2] ] -> return 24
+
+'''
+
+'''
+create a function name fib that a number and calculate the fibonacci sequence 
+
+fib(0) -> 1
+fib(1) -> 1
+fib(2) -> 2
+fib(3) -> 3
+fib(4) -> 5
+fib(5) -> 8
+fib(6) -> 13
+fib(7) -> 21
+'''
