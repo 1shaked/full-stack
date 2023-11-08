4e36cc127f64f313151e53625d671150172f82fc
1shaked <40497915+1shaked@users.noreply.github.com>
2023-11-08 20:09:34 +0200

    blog item

--- 29ass/nodeProjectLess64/backend/server/trpc/blog/index.ts
+++ 29ass/nodeProjectLess64/backend/server/trpc/blog/index.ts
@@ -2,12 +2,14 @@ import { router } from "../trpc";
 import { CommentRouterTrpc } from "./comment";
 import { blogList } from "./blogList";
 import { blogCreate } from "./blogCreate";
+import { blogItem } from "./blogItem";
 
 export const BlogRouterTrpc = router({
   comment: CommentRouterTrpc,
   list: blogList,
   // to add data to the database
   create: blogCreate,
+  item: blogItem,
 });
 
 /**
--- 29ass/nodeProjectLess64/frontend/newBlog/src/routes/blog/_id.tsx
+++ 29ass/nodeProjectLess64/frontend/newBlog/src/routes/blog/_id.tsx
@@ -1,8 +1,21 @@
 import { useParams } from "react-router-dom"
+import { trpc } from "../../trpc";
 
 export function BlogIdPage() {
-    const { id } = useParams()
+    const { id } = useParams();
+    const id_num = parseInt(id ?? '0');
+    if (isNaN(id_num) || id_num < 0) return <div>error</div>
+    const blogQuery = trpc.blog.item.useQuery({ id: id_num});
+
+    if (blogQuery.isLoading) return <div>Loading...</div>
+    if (blogQuery.data?.blogData == null || blogQuery.data?.blogData == undefined) return <div>This page does not exists</div>
     return <div>
-        <h1>blog details - { id } </h1>
+        <h1>{blogQuery.data.blogData.title} - { id } </h1>
+        <p>
+            {blogQuery.data.blogData.content}
+        </p>
+        {blogQuery.data?.blogComments.map((comment, index) => <div key={index}>
+            {comment.comment}
+        </div>)}
     </div>
 }
