7e38d26a2b5e140458d961684dad19f2c96deb01
1shaked <shaked1hen@gmail.com>
2023-11-13 18:45:19 +0200

    added delete

--- 29ass/nodeProjectLess64/backend/server/trpc/blog/index.ts
+++ 29ass/nodeProjectLess64/backend/server/trpc/blog/index.ts
@@ -3,12 +3,14 @@ import { CommentRouterTrpc } from "./comment";
 import { blogList } from "./blogList";
 import { blogCreate } from "./blogCreate";
 import { blogItem } from "./blogItem";
+import { blogDelete } from "./blogDelete";
 
 export const BlogRouterTrpc = router({
   comment: CommentRouterTrpc,
   list: blogList,
   // to add data to the database
   create: blogCreate,
+  delete: blogDelete,
   item: blogItem,
 });
 
--- 29ass/nodeProjectLess64/frontend/newBlog/src/routes/blog/page.tsx
+++ 29ass/nodeProjectLess64/frontend/newBlog/src/routes/blog/page.tsx
@@ -3,14 +3,23 @@ import { trpc } from "../../trpc"
 import './blogList.css'
 export function BlogPage() {
     const blogQuery = trpc.blog.list.useQuery();
+    const deleteBlogMutation = trpc.blog.delete.useMutation({
+        onSuccess: () => {
+            blogQuery.refetch();
+        }
+    })
     if (blogQuery.isLoading) return <div>Loading...</div>
     return <div>
         <h1>blog list</h1>
         <NavLink to={'/blog/add'} className='AddBlogLink'>add blog</NavLink>
         <hr />
-        {blogQuery.data?.map((blog, index) => <NavLink className="blogList" key={index} to={`/blog/${blog.id}`}>
-            {blog.title}
-        </NavLink>)}
+        {blogQuery.data?.map((blog, index) => <div key={index} className='blogListRow'>
+            <NavLink className="blogList" to={`/blog/${blog.id}`}>
+                {blog.title}
+            </NavLink>
+            <button onClick={() => deleteBlogMutation.mutate({id: blog.id})} className='DeleteBlogBtn'>delete blog</button>
+        </div>)}
+        
     </div>
 }
 /**
