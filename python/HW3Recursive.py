55368f946b9771eae55dbd11a82cdfea8f665f9d
1shaked <shaked1hen@gmail.com>
2023-11-14 21:24:39 +0200

    more questions

--- python/HW3Recursive.py
+++ python/HW3Recursive.py
@@ -16,11 +16,47 @@ fib(x) = fib(x - 1) + fib(x - 2)
 '''
 
 '''
-create a recursive function that calculates counts the total numbers of sub arrays
+create a recursive function that counts the total numbers of sub arrays
 
 countArray([ [] ]) -> 2
+countArray([ [ [] ] ]) -> 3
 countArray([ [] , [] ]) -> 3
+countArray([ [4] , [2] ]) -> 3
+countArray([ [4] , [9], 100 ]) -> 3
+countArray([ [-1], 100 , [200], 3 ]) -> 3
 countArray([ [] , [ [] ] ]) -> 4
 countArray([ [] , [ [] ] ], []) -> 4
+countArray([ [] , [ [] ] ], [], []) -> 5
+countArray([ [] , [ [] ] ], [], [ [] ]) -> 6
+countArray([ [] , [ [] ] ], [], [ [ [] ] ]) -> 8
+
+'''
+
+'''
+create a recursive function that counts how many sub objects are there
+
+countSubObject( {} ) -> 1
+countSubObject( { 'a': {} } ) -> 2
+countSubObject( { 'a': {}, 'b': {} } ) -> 3
+countSubObject( { 'a': {}, 'b': { 'c': 2 } } ) -> 3
+countSubObject( { 'a': {}, 'b': { 'c':  {} } } ) -> 4
+'''
+
+'''
+write a recursive function search (binary) function that take a number and search for it in the sorted array
+
+findInArray([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) -> 2 
+findInArray([1, 2, 3, 4, 5, 6, 7, 8, 9], 5) -> 6 
+findInArray([1, 2, 3, 4, 5, 6, 7, 8, 9], 5.5) -> -1 
+'''
+
+'''
+create a recursive function that counts the max depth numbers of sub arrays
+
+countMaxDepth([ [] ] ) -> 1
+countMaxDepth([ [] , [ [] ] ] ) -> 2
+countMaxDepth([ [] , [ [] ] ] , [ [ [] ] ]) -> 3
+countMaxDepth([ [] , [ [] ] ] , [ [ ] ]) -> 2
+countMaxDepth([ [ [ [4 , [] ] ] ] , [ [] ] ] , [ [ ] ]) -> 4
 
 '''
