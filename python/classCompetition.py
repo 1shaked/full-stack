4b11779a6f923920348d6da9d4ba553faa5f7e9c
1shaked <shaked1hen@gmail.com>
2023-11-14 18:43:06 +0200

    performance improvement

--- python/classCompetition.py
+++ python/classCompetition.py
@@ -20,9 +20,24 @@ def getIndexOfNumberInArrayWithWhile(arr: list[int], searchNumber: int) -> int:
     return -1
 
 
+def getIndexOfNumberInArrayWithWhileFast(arr: list[int], searchNumber: int) -> int:
+    index = 0
+    len_arr = len(arr)
+    index_middle = int(len_arr / 2)
+    if arr[index_middle] < searchNumber: # the number is to the right
+        index = index_middle
 
+    while index < len_arr:
+        element = arr[index] 
+        if (element == searchNumber):
+            return index
+        index = index + 1
+    return -1
+
+
+arr = [i for i in range(10**9)]
 start_time = time.time()
-getIndexOfNumberInArray(arr=[i for i in range(10**8)], searchNumber=850000)
+getIndexOfNumberInArrayWithWhileFast(arr=arr, searchNumber=(10**9)/2 + 3)
 end_time = time.time()
 execution_time = end_time - start_time
 print(f"The function executed in {execution_time} seconds.")
