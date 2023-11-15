b7cf26036d74f8cbf8c0f702698152fef428b957
1shaked <shaked1hen@gmail.com>
2023-11-15 19:14:06 +0200

    added react query

--- 31ass/chat-app/src/App.tsx
+++ 31ass/chat-app/src/App.tsx
@@ -1,21 +1,25 @@
 import './App.css'
 import { getDocs, collection } from 'firebase/firestore'
 import { firebaseDB } from './firebase_connection'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 function App() {
   async function readData() {
     const collectionTest = collection(firebaseDB, 'test')
     const querySet = await getDocs(collectionTest);
     // console.log(querySet)
-    for (const doc of querySet.docs) {
+    for (const item of querySet.docs) {
 
-      console.log(doc.data())
+      console.log(item.data())
     }
   }
   readData();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <h1>Shaked</h1>
-    </>
+    </QueryClientProvider>
   )
 }
 
