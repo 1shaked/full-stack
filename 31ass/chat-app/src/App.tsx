bd3f5adf93f1f0bfd6c206b554f6600529da8601
1shaked <shaked1hen@gmail.com>
2023-11-15 19:29:57 +0200

    display the data

--- 31ass/chat-app/src/App.tsx
+++ 31ass/chat-app/src/App.tsx
@@ -1,24 +1,17 @@
 import './App.css'
-import { getDocs, collection } from 'firebase/firestore'
-import { firebaseDB } from './firebase_connection'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { DisplayExample } from './components/displayExample'
 
 const queryClient = new QueryClient()
 
 function App() {
-  async function readData() {
-    const collectionTest = collection(firebaseDB, 'test')
-    const querySet = await getDocs(collectionTest);
-    // console.log(querySet)
-    for (const item of querySet.docs) {
-
-      console.log(item.data())
-    }
-  }
-  readData();
   return (
     <QueryClientProvider client={queryClient}>
+      <>
       <h1>Shaked</h1>
+      <DisplayExample />
+      
+      </>
     </QueryClientProvider>
   )
 }
