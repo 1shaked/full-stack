0cabb6065850b8f4a8b4801960464f236da6bfaf
1shaked <shaked1hen@gmail.com>
2023-11-15 18:28:42 +0200

    reading the data

--- 31ass/chat-app/src/App.tsx
+++ 31ass/chat-app/src/App.tsx
@@ -1,7 +1,17 @@
 import './App.css'
-
+import { getDocs, collection } from 'firebase/firestore'
+import { firebaseDB } from './firebase_connection'
 function App() {
+  async function readData() {
+    const collectionTest = collection(firebaseDB, 'test')
+    const querySet = await getDocs(collectionTest);
+    // console.log(querySet)
+    for (const doc of querySet.docs) {
 
+      console.log(doc.data())
+    }
+  }
+  readData();
   return (
     <>
       <h1>Shaked</h1>
