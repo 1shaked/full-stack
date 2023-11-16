28cbeff6e466eebc69b431b5b0f4e0cff6a6d902
1shaked <40497915+1shaked@users.noreply.github.com>
2023-11-16 18:35:16 +0200

    while loop

---
+++ js/loop_arr.js
@@ -0,0 +1,10 @@
+const arr = ['apple', 5, 'MSI', true]
+//             0      1    2      3
+// arr.length = 4
+
+// while loop
+let index = 0
+while (index < arr.length) {
+    console.log(`The value at ${index} is ${arr[index]}`)
+    index = index + 1
+}
---
+++ python/loop_arr.py
@@ -0,0 +1,9 @@
+arr = ['apple', 5 , 'MSI', True]
+#        0      1    2      3
+# len(arr) = 4
+
+# while loop
+index = 0
+while index < len(arr):
+    print(f'The value at {index} is {arr[index]}')
+    index = index + 1
