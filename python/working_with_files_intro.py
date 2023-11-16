c46e1085038f44730f1b738c93e1e093b1631944
1shaked <40497915+1shaked@users.noreply.github.com>
2023-11-16 20:27:27 +0200

    files read

---
+++ python/working_with_files_intro.py
@@ -0,0 +1,13 @@
+# read file 
+with open(r'python/files/data.txt', 'r') as f:
+    # print(f.read())
+    # lines = f.readlines()
+    # print(lines)
+    print(f.readline())
+    print(f.readline())
+    print(f.readline())
+    # print(f.readline())
+
+
+# with open(r'python/files/data.txt', 'w') as f:
+#     f.write('wowowowowo wwwo')
