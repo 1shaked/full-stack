b2b107bd05c43cd563a8ce69797357227d333cdf
1shaked <40497915+1shaked@users.noreply.github.com>
2023-11-16 20:14:11 +0200

    fib rec

--- js/functions_fib.js
+++ js/functions_fib.js
@@ -8,5 +8,17 @@ function fib(index) {
     // arr[2] = arr[1] + arr[0]
     return arr.at(-1)
 }
+/**
+ * 
+ * @param {number} index 
+ */
+function fib_rec(index) {
+    if (index == 0 || index == 1) {
+        return 1;
+    }
+    const prev = fib_rec(index - 1)
+    const prev_of_prev = fib_rec(index - 2)
+    return prev + prev_of_prev
+}
 
-fib(index=7)
+// fib(index=7)
--- python/functions_fib.py
+++ python/functions_fib.py
@@ -6,7 +6,18 @@ def fib(index: int):
     for currentIndex in range(2, index + 1):
         arr.append(arr[-1] + arr[-2])
 
-    # arr[2] = arr[1] + arr[0]
     return arr[-1]
 
-fib(index = 7)
+
+
+
+def fib_rec(index: int):
+    if index == 0 or index == 1:
+        return 1
+    prev = fib_rec(index - 1)
+    prev_of_prev = fib_rec(index - 2)
+    return prev + prev_of_prev
+
+# print(fib(index = 7))
+
+# print(fib_rec(7))
