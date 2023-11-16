e4515c7293e7da50fa3ca5147bb28374c84c1d80
1shaked <40497915+1shaked@users.noreply.github.com>
2023-11-16 17:52:50 +0200

    string format obj

---
+++ js/string_format_obj.js
@@ -0,0 +1,8 @@
+let obj =  {
+    'name': 'shaked',
+    'age': 24,
+    'hobbies': ['soccer', 'math', 'what ever']
+}
+
+console.log(obj['age'])
+console.log(`the obj age is ${obj['age']} and have a nice day`)
