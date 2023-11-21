0c0b1897c6623855328d3b1bc49f3ad2e5ccf98e
1shaked <shaked1hen@gmail.com>
2023-11-21 19:30:28 +0200

    methods basic

--- js/OOP/methods.js
+++ js/OOP/methods.js
@@ -9,7 +9,14 @@ class Person {
     greet() {
         console.log(`Hello ${this.name}`)
     }
-}
 
+    incrementAge() {
+        this.age = this.age + 1;
+    }
+}
+// create the object from the blueprint
 const p1 = new Person('shaked1', 23)
 p1.greet()
+console.log(p1.age)
+p1.incrementAge()
+console.log(p1.age)
--- python/OOP/methods.py
+++ python/OOP/methods.py
@@ -5,8 +5,14 @@ class Person :
         self.age = ageOfPerson
 
     def greet(self):
-        print(f'Hello {self.name}')    
+        print(f'Hello {self.name}')
+
+    def incrementAge(self):
+        self.age = self.age + 1
 
 # create the object from the blueprint
 p1 = Person('y', 24)
+print(p1.age)
+p1.incrementAge()
+print(p1.age)
 p1.greet()
