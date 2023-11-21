b72f6c0058fe9f4269e37f441fe5dd843456896e
1shaked <shaked1hen@gmail.com>
2023-11-21 19:17:04 +0200

    add prop to object

---
+++ python/OOP/add_prop_to_object.py
@@ -0,0 +1,18 @@
+# size , wheels , company  , ...
+
+# drive , reverse , park , N , bth  , ac ...
+
+# create blueprint
+class Person :
+    def __init__(self, nameOfPerson, ageOfPerson):
+        self.name = nameOfPerson
+        self.age = ageOfPerson
+    
+
+# create the object from the blueprint
+p1 = Person('shaked', 24)
+print(p1.name)
+p1.name = 'John'
+p1.x = 'yyyy'
+print(p1.name)
+print(p1.age)
