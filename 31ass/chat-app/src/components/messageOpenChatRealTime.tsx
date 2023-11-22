8163d5d53ebb34b8e72ba7b13535a51a3d2e1f3b
1shaked <40497915+1shaked@users.noreply.github.com>
2023-11-22 20:25:37 +0200

    real time updates

--- 31ass/chat-app/src/components/messageOpenChatRealTime.tsx
+++ 31ass/chat-app/src/components/messageOpenChatRealTime.tsx
@@ -1,10 +1,10 @@
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { doc, setDoc, onSnapshot } from "firebase/firestore";
 import { firebaseDB } from "../firebase_connection";
-import { useQuery } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
 import { useAtomValue } from "jotai";
 import { isLoggedInAtom, userAtom } from "../userState";
 import { getCurrentDateFormatted, getCurrentTimeFormatted } from "../utils/getCurrentTime";
+import { useEffect, useState } from "react";
 interface FormMessageInterface {
     message: string;
 }
@@ -22,28 +22,39 @@ interface MessagesOpenChatInterface {
         }
     }
 }
-async function readMessages() {
-    const docRef = doc(firebaseDB, 'messages', getCurrentDateFormatted());
-    const docSnap = await getDoc(docRef);
-    console.log(getCurrentDateFormatted())
-    console.log(docSnap.metadata);
-    console.log(docSnap.data());
-    return {data: docSnap.data(), id: docSnap.id} as MessagesOpenChatInterface
-}
+
 export function MessagesOpenChatRealTime() {
-    const messagesQuery = useQuery({
-        queryKey: ['messagesOpenChatRealTime'],
-        queryFn: readMessages
-    });
+
+    const [openChatInfo, setOpenChatIInfo] = useState<MessagesOpenChatInterface | null>(null);
     const isLoggedIn = useAtomValue(isLoggedInAtom)
     const user = useAtomValue(userAtom)
     const {handleSubmit, register} = useForm<FormMessageInterface>()
-    if (messagesQuery.isLoading) return <div>Loading...</div>
+    
+    useEffect(() => {
+        const stream = onSnapshot(doc(firebaseDB, 'messages', getCurrentDateFormatted()), (docRef) => {
+            const data = {data: docRef.data(), id: docRef.id} as MessagesOpenChatInterface;
+            setOpenChatIInfo(data);
+        });
+        return () => {
+            stream();
+        }
+    }, [])
+    
     if (!isLoggedIn) return <div>you are not isLoggedIn</div>
     return <div>
-        <h1>open chat</h1>
+        <h1>open chat - {user?.email}</h1>
         <div>
-            {Object.entries(messagesQuery.data?.data.messages ?? {}).map((item, index) => {
+            {Object.entries(openChatInfo?.data.messages ?? {}).sort((first_item , second_item) =>  {
+                const local_key_time_first = first_item[0]
+                const local_key_time_second = second_item[0];
+                const [hoursA, minutesA, secondsA] = local_key_time_first.split(':').map(Number);
+                const [hoursB, minutesB, secondsB] = local_key_time_second.split(':').map(Number);
+
+                const totalSecondsA = hoursA * 3600 + minutesA * 60 + secondsA;
+                const totalSecondsB = hoursB * 3600 + minutesB * 60 + secondsB;
+
+                return totalSecondsA - totalSecondsB;
+            }).map((item, index) => {
                 return <div key={index}>
                     {item[0]} --- {item[1].sender} - {item[1].message}
                 </div>
@@ -51,16 +62,14 @@ export function MessagesOpenChatRealTime() {
         </div>
         <form onSubmit={handleSubmit( async (message) => {
             // const collectionMessage = collection(firebaseDB, 'messages');
-            if (messagesQuery.data === undefined) return ;
-            const dataToSave = {...messagesQuery.data.data};
+            if (openChatInfo === null) return ;
+            const dataToSave = {...openChatInfo.data};
             
             dataToSave.messages[getCurrentTimeFormatted()] = {
                 sender: user?.email ?? '',
                 message: message.message
             }
-            // await addDoc(collectionMessage, dataToSave);
-            await setDoc(doc(firebaseDB, 'messages', messagesQuery.data.id) , dataToSave)
-            messagesQuery.refetch()
+            await setDoc(doc(firebaseDB, 'messages', openChatInfo.id) , dataToSave)
         })}>
             <input type="text" placeholder="message" {...register('message')} />
             <button type="submit">send</button>
