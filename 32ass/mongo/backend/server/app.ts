84d20aa3f0d327164dfcac356bff289a5b3afba3
1shaked <shaked1hen@gmail.com>
2023-11-27 18:44:12 +0200

    Add backend server and package.json for MongoDB project

---
+++ 32ass/mongo/backend/server/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors'
+
+
+const app = express(); // create the server and save the ref in the app variable
+
+app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:5173']
+}));
+
+app.get('/' , (req, res) => {
+    res.send({
+        message: 'started my project'
+    })
+})
+
+app.listen(3301, () => {
+    console.log('listening on 3301')
+})
