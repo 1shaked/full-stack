aed75239d87ec94ac41dc0a8dd50c0d614dd5cd9
1shaked <40497915+1shaked@users.noreply.github.com>
2023-11-29 18:59:03 +0200

    addded author

--- 32ass/mongo/backend/server/trpc/index.ts
+++ 32ass/mongo/backend/server/trpc/index.ts
@@ -1,10 +1,16 @@
+import { prismaDB } from '../connection';
 import { publicProcedure, router } from './trpc';
 
 export const appRouter = router({
   // ...
   posts: publicProcedure.query(async () => {
-    return [10, 20 ,30, 40];
-  })
+    
+    return prismaDB.post.findMany();
+  }),
+  authors: publicProcedure.query(async () => {
+    
+    return prismaDB.author.findMany();
+  }),
 });
 // Export type router type signature,
 // NOT the router itself.
--- 32ass/mongo/frontend/mongo-project/src/components/example.tsx
+++ 32ass/mongo/frontend/mongo-project/src/components/example.tsx
@@ -1,11 +1,11 @@
 import { trpc } from "../trpc";
 
 export function Example() {
-    const post_query = trpc.posts.useQuery();
-    if (post_query.isLoading) return <div>loading</div>
+    const author_query = trpc.authors.useQuery()
+    if (author_query.isLoading) return <div>loading</div>
     return <div>
         <pre>
-            {JSON.stringify(post_query.data, null , 2)}
+            {JSON.stringify(author_query.data, null , 2)}
         </pre>
     </div>
 
