338e75ba69dab91f60fb23726b20042041b15351
1shaked <shaked1hen@gmail.com>
2023-11-30 20:21:40 +0200

    basic for python and js

---
+++ js/arr/arr_basic_loop.js
@@ -0,0 +1,5 @@
+let arr = ['a' , 'b' , 'c' , 'd', -1  , -2]
+
+for (const item of arr) {
+    console.log(item)
+}
---
+++ python/arr/arr_basic_loop.py
@@ -0,0 +1,5 @@
+
+arr = ['a' , 'b' , 'c' , 'd', -1  , -2]
+
+for item in arr:
+    print(item)
