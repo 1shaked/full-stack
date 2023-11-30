b866e37aa4b11cda5fcbc0febcb10b39c2e90901
1shaked <shaked1hen@gmail.com>
2023-11-30 21:02:45 +0200

    function js and python

---
+++ js/functions/functions_basic.js
@@ -0,0 +1,16 @@
+
+function greet(name) {
+    console.log(`Hello mr/ms ${name}`)
+}
+
+function power_to(n1, p) {
+    return Math.pow(n1, p)
+}
+// greet('Shaked')
+// greet('Chen')
+// greet('Dror')
+
+
+let five_pow_two = power_to(5, 2)
+console.log(five_pow_two)
+console.log(power_to(5, 3))
