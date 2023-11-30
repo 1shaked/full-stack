fdd0673f93d8dadeb299021e4475c50ac4aca70c
1shaked <shaked1hen@gmail.com>
2023-11-30 20:25:32 +0200

    arr basic options

--- js/arr/arr_basic_loop.js
+++ js/arr/arr_basic_loop.js
@@ -3,3 +3,13 @@ let arr = ['a' , 'b' , 'c' , 'd', -1  , -2]
 for (const item of arr) {
     console.log(item)
 }
+
+let index = 0;
+while (index < arr.length) {
+    console.log(arr[index])
+    index = index + 1 // index++;
+}
+
+for (let index = 0; index < arr.length; index++) {
+    console.log(arr[index])
+}
