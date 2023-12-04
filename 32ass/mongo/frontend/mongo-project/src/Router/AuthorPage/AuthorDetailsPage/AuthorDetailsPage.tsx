00dcaae047ba03377a17629bdae75a8a2c3b958d
1shaked <40497915+1shaked@users.noreply.github.com>
2023-12-04 17:24:44 +0200

    display better

--- 32ass/mongo/frontend/mongo-project/src/Router/AuthorPage/AuthorDetailsPage/AuthorDetailsPage.tsx
+++ 32ass/mongo/frontend/mongo-project/src/Router/AuthorPage/AuthorDetailsPage/AuthorDetailsPage.tsx
@@ -4,13 +4,24 @@ import { trpc } from "../../../trpc";
 export function AuthorDetailsPage() {
     const params = useParams()
     const {id} = params;
-    const author_details = trpc.author.details.useQuery(id ?? '')
+    const author_details_query = trpc.author.details.useQuery(id ?? '')
     if (id === undefined) return <main>Error no id was provided</main>
+    if (author_details_query.isLoading) return <main>Loading ... </main>
+    if (author_details_query.data === undefined || author_details_query.data.authorDetails === null) return <h1>No author with this details</h1>
     return <main>
         <h1>AuthorDetailsPage</h1>
         id is {JSON.stringify(params, null , 2)}
+        <div>
+            {author_details_query.data.authorDetails.name}
+            <p>
+                {author_details_query.data.authorDetails.bio}
+            </p>
+        </div>
+        <div>
+            
+        </div>
         <pre>
-            {JSON.stringify(author_details.data, null, 2 )}
+            {JSON.stringify(author_details_query.data, null, 2 )}
         </pre>
     </main>
 }
