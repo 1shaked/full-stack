7e6c38e2c866b0c1abc0ef0e817d1de0ca5dd0a3
1shaked <40497915+1shaked@users.noreply.github.com>
2023-12-04 18:00:39 +0200

    Fix bookRouter export

--- 32ass/mongo/backend/server/trpc/bookRouter/index.ts
+++ 32ass/mongo/backend/server/trpc/bookRouter/index.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 import {publicProcedure, router} from '../trpc'
 import { prismaDB } from '../../connection'
 
-export const BookRouter = router({
+export const BookRouter = router({//
     bookDetails: publicProcedure.input(z.string()).query(async (opts) => {
         const book = await prismaDB.book.findFirst({
             where: {
