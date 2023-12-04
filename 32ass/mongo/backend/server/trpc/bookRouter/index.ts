d3405ab05fa9bf5ca1f79e44b54bf2a320ca52d0
1shaked <40497915+1shaked@users.noreply.github.com>
2023-12-04 20:44:48 +0200

    remove imports

--- 32ass/mongo/backend/server/trpc/bookRouter/index.ts
+++ 32ass/mongo/backend/server/trpc/bookRouter/index.ts
@@ -1,6 +1,4 @@
-import { z } from 'zod'
-import {publicProcedure, router} from '../trpc'
-import { prismaDB } from '../../connection'
+import {router} from '../trpc'
 import { bookCreate } from './book_create'
 import { bookList } from './book_list'
 import { bookDetails } from './book_details'
