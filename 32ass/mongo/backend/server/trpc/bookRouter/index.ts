07a881a9b83a95d0f87d99abc31218aa03c157fe
1shaked <40497915+1shaked@users.noreply.github.com>
2023-12-04 17:46:06 +0200

    book router

---
+++ 32ass/mongo/backend/server/trpc/bookRouter/index.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+import {publicProcedure, router} from '../trpc'
+import { prismaDB } from '../../connection'
+
+export const BookRouter = router({
+    book: publicProcedure.input(z.string()).query(async (opts) => {
+        const book = await prismaDB.book.findFirst({
+            where: {
+                id: opts.input
+            }
+        })
+        return book;
+    })
+})
