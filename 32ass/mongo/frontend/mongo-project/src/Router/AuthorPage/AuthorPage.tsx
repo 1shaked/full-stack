880267eb30c1ebd2d8ef18774e549f36bb5938c1
1shaked <40497915+1shaked@users.noreply.github.com>
2023-12-04 17:24:34 +0200

    added link to each page

--- 32ass/mongo/frontend/mongo-project/src/Router/AuthorPage/AuthorPage.tsx
+++ 32ass/mongo/frontend/mongo-project/src/Router/AuthorPage/AuthorPage.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import { trpc } from "../../trpc"
 
 export function AuthorPage() {
@@ -7,7 +8,13 @@ export function AuthorPage() {
         <h1>AuthorPage page</h1>
         <div>
             {author_query.data?.map((author, index) => <div key={index}>
-                {JSON.stringify(author, null , 2 )}
+                {/* {JSON.stringify(author, null , 2 )} */}
+                <h2>
+                    <NavLink to={`/author/${author.id}`}>{author.name}</NavLink>
+                </h2>
+                <p>
+                    {author.bio}
+                </p>
                 <hr />
             </div>)}
         </div>
