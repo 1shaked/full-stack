6e450a77e0e69587b1442497a0a0a41309488634
1shaked <shaked1hen@gmail.com>
2023-12-06 19:04:35 +0200

    created tje house page

---
+++ 32ass/mongo/frontend/mongo-project/src/Router/HousePage/HouseDetails/HouseDetails.tsx
@@ -0,0 +1,20 @@
+import { useParams } from "react-router-dom"
+import { trpc } from "../../../trpc"
+
+
+export function HouseDetailsPage () {
+    const {id} = useParams();
+    const home_details_query = trpc.home.get.useQuery(id ?? '');
+    if (home_details_query.isLoading) return <h1>Loading...</h1>
+    if (home_details_query.data === undefined) return <h1>Page does not exist</h1>
+    return <main>
+        <h1>House Details</h1>
+        {home_details_query.data.address}
+        <hr />
+        {home_details_query.data.city}
+        <hr />
+        personId - {home_details_query.data.personId}
+    </main>
+}
+
+
