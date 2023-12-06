ae4896c958de48a80a26888ebc3f23fea59e5567
1shaked <shaked1hen@gmail.com>
2023-12-06 20:54:58 +0200

    added the person router tp the index page

--- 32ass/mongo/backend/server/trpc/index.ts
+++ 32ass/mongo/backend/server/trpc/index.ts
@@ -2,10 +2,12 @@ import { router } from './trpc';
 import {AuthorRouter} from './authorRouter'
 import { BookRouter } from './bookRouter';
 import { homeRouter } from './homeRouter';
+import { personRouter } from './personRouter';
 export const appRouter = router({
   author: AuthorRouter,
   book: BookRouter,
-  home: homeRouter
+  home: homeRouter,
+  person: personRouter
 });
 
 // Export type router type signature,
