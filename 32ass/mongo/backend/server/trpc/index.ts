83b8ebc6491cb4a10b76b054339073c2c6691a55
1shaked <shaked1hen@gmail.com>
2023-12-06 18:04:58 +0200

    added home router trpc

--- 32ass/mongo/backend/server/trpc/index.ts
+++ 32ass/mongo/backend/server/trpc/index.ts
@@ -1,10 +1,11 @@
-import { prismaDB } from '../connection';
-import { publicProcedure, router } from './trpc';
+import { router } from './trpc';
 import {AuthorRouter} from './authorRouter'
 import { BookRouter } from './bookRouter';
+import { homeRouter } from './homeRouter';
 export const appRouter = router({
   author: AuthorRouter,
   book: BookRouter,
+  home: homeRouter
 });
 
 // Export type router type signature,
