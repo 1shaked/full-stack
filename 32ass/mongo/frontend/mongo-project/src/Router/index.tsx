770b1e995b87ecc0bfbfc502b3a594857397321c
1shaked <40497915+1shaked@users.noreply.github.com>
2023-12-13 20:31:42 +0200

    added person page and person list

--- 32ass/mongo/frontend/mongo-project/src/Router/index.tsx
+++ 32ass/mongo/frontend/mongo-project/src/Router/index.tsx
@@ -8,6 +8,7 @@ import { BookDetailsPage } from "./BooksPage/BookDetailsPage/BookDetailsPage";
 import { HousePage } from "./HousePage/HousePage";
 import { HouseDetailsPage } from "./HousePage/HouseDetails/HouseDetails";
 import { PersonsPage } from "./Persons/PersonsPage";
+import { PersonsDetailsPage } from "./Persons/PersonsDetails/PersonsDetailsPage";
 export function RouterComponent() {
     return <>
     <Routes>
@@ -19,7 +20,8 @@ export function RouterComponent() {
         <Route path="/books/:id" element={<BookDetailsPage />}></Route>
         <Route path='/house' element={<HousePage />}></Route>
         <Route path='/house/:id' element={<HouseDetailsPage />}></Route>
-        <Route path="/persons" element={<PersonsPage/ >}></Route>
+        <Route path="/persons" element={<PersonsPage />}></Route>
+        <Route path="/persons/:id" element={<PersonsDetailsPage />}></Route>
     </Routes>
     </>
 }
