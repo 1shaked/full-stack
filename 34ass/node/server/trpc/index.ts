de5e57ff5a91f70760668cd69799b6d0ebe5c3ec
1shaked <40497915+1shaked@users.noreply.github.com>
2023-12-17 19:37:46 +0200

    added delete and create function

--- 34ass/node/server/trpc/index.ts
+++ 34ass/node/server/trpc/index.ts
@@ -1,13 +1,35 @@
 import { prisma } from '../prisma';
 import { publicProcedure, router } from './trpc';
 import {z} from 'zod'
+export const ZodUserSchema = z.object({
+  email: z.string().email(),
+  name: z.string(),  
+});
+
+export type UserSchemaType = z.infer<typeof ZodUserSchema>;
+
 export const appRouter = router({
-  userList: publicProcedure.input(z.string()).query(async (opts) => {
-    console.log(opts.input)
+  userList: publicProcedure.query(async (opts) => {
+    
     const users = await prisma.user.findMany()
     console.log(users)
     return users
-  })
+  }),
+  userCreate: publicProcedure.input(ZodUserSchema).mutation(async (opts) => {    
+    const newUser = await prisma.user.create({
+      data: opts.input
+    });
+    return newUser
+  }),
+  userDelete: publicProcedure.input(z.number()).mutation(async (opts) => {  
+    const deleteUser = await prisma.user.delete({
+      where: {
+        id: opts.input
+      }
+    });
+    return deleteUser
+  }),
+
 });
 
 // Export type router type signature,
--- 34ass/posts-example/src/components/usersList.tsx
+++ 34ass/posts-example/src/components/usersList.tsx
@@ -1,12 +1,42 @@
+import { useForm } from "react-hook-form";
 import { trpc } from "../trpc"
 
+interface UserCreateInterface {
+    name: string;
+    email: string;
+}
+
 export function UsersList() {
-    const users_list_query = trpc.userList.useQuery('wow!');
+    const users_list_query = trpc.userList.useQuery();
+    const user_add_mutation = trpc.userCreate.useMutation({
+        onSuccess: () => {
+            users_list_query.refetch()
+        }
+    });
+    const user_delete_mutation = trpc.userDelete.useMutation({
+        onSuccess: () => {
+            users_list_query.refetch()
+        }
+    })
+    const {register , handleSubmit} = useForm<UserCreateInterface>()
     if (users_list_query.isLoading) return <div>Loading....</div>
     return <>
         <h1>users list</h1>
         {users_list_query.data?.map((user, index) => <div key={index}>
-            {user.email}
+            {user.email} - <button onClick={() => user_delete_mutation.mutate(user.id)} style={{color: 'rgb(230, 0 ,0 )'}}>DELETE</button>
+            <hr />
         </div>)}
+
+        <hr />
+
+        <form onSubmit={handleSubmit((data) => user_add_mutation.mutate(data))}>
+            name
+            <input type="text" {...register('name')} />
+            <br />
+            email
+            <input type="text" {...register('email')} />
+
+            <button type="submit">submit</button>
+        </form>
     </>
 }
