05d81b6176181c7bbaa675cdcbe7fe98df4869de
1shaked <40497915+1shaked@users.noreply.github.com>
2023-12-20 19:53:47 +0200

    added redux counter

--- 29ass/nodeProjectLess64/frontend/newBlog/src/components/redux_counter.tsx
+++ 29ass/nodeProjectLess64/frontend/newBlog/src/components/redux_counter.tsx
@@ -1,6 +1,6 @@
 
 import { useSelector } from "react-redux"
-import {incremented } from '../redux/counterSlice'
+import {counterSlice } from '../redux/counterSlice'
 import { useDispatch } from "react-redux"
 import { RootState } from "../redux/store"
 export function ReduxCounter() {
@@ -9,8 +9,8 @@ export function ReduxCounter() {
     
     return <div>
         <h1>redux start!</h1>
-        <button onClick={() => dispatch(incremented())}>+</button>
-        <button onClick={() => dispatch(incremented(2))}>+2</button>
+        <button onClick={() => dispatch(counterSlice.actions.incremented())}>+</button>
+        <button onClick={() => dispatch(counterSlice.actions.incremented(2))}>+2</button>
         <p>
             {counter.value}
         </p>
