dde72da0b62ad06e5f3aa8c1bd172017dba9cd4a
1shaked <40497915+1shaked@users.noreply.github.com>
2023-12-25 17:33:14 +0200

    register func

--- 29ass/nodeProjectLess64/backend/server/trpc/user/index.ts
+++ 29ass/nodeProjectLess64/backend/server/trpc/user/index.ts
@@ -1,6 +1,8 @@
 import {router} from '../trpc'
+import { createUser } from './createUser'
 import { getUser } from './getUser'
 
 export const UserRouterTrpc = router({
-    get: getUser
+    get: getUser,
+    create: createUser
 })
