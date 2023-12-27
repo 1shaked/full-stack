2a01ba2e199ab85a9885040bb263afe346643683
1shaked <40497915+1shaked@users.noreply.github.com>
2023-12-27 19:30:30 +0200

    create the hash with salt functions

--- 29ass/nodeProjectLess64/backend/server/trpc/user/createUser.ts
+++ 29ass/nodeProjectLess64/backend/server/trpc/user/createUser.ts
@@ -1,7 +1,31 @@
 import { z } from "zod";
 import { publicProcedure } from "../trpc";
 import { prisma } from "../../connection";
+import * as bcrypt from 'bcrypt';
 
+
+//////////////////////////////
+const saltRounds = 10;
+
+async function hashPassword(password: string): Promise<string> {
+    try {
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        return hashedPassword;
+    } catch (error) {
+        throw new Error('Error hashing password: ' + error);
+    }
+}
+
+async function checkPassword(password: string, hashedPassword: string): Promise<boolean> {
+    try {
+        const match = await bcrypt.compare(password, hashedPassword);
+        return match;
+    } catch (error) {
+        throw new Error('Error checking password: ' + error);
+    }
+}
+//////////////////////////////
 
 export const createUser = publicProcedure.input(z.object({
     email: z.string(),
