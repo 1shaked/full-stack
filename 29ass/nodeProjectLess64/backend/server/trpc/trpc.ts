7fe843d06f003d6ceeda3f07dbddbec8c3df9511
1shaked <40497915+1shaked@users.noreply.github.com>
2024-01-01 17:44:41 +0200

    added login middle ware

--- 29ass/nodeProjectLess64/backend/server/trpc/trpc.ts
+++ 29ass/nodeProjectLess64/backend/server/trpc/trpc.ts
@@ -1,6 +1,7 @@
-import { initTRPC } from '@trpc/server';
+import { TRPCError, initTRPC } from '@trpc/server';
 import { Context } from '../utils/trpcExpressContent';
-
+import {z} from 'zod'
+import { prisma } from '../connection';
 /**
  * Initialization of tRPC backend
  * Should be done only once per backend!
@@ -9,8 +10,45 @@ const t = initTRPC.context<Context>().create();
 
 export const example_middleware = t.middleware(async (opts) => {
     console.log(`example_middleware, ${opts.path}`);
-    console.log(opts.ctx.req.cookies)
-    opts.ctx.res.cookie('session', 'chen')
+    // console.log(opts.ctx.req.cookies)
+    // opts.ctx.res.cookie('session', 'chen')
+    return opts.next();
+});
+
+export const login_middleware = t.middleware(async (opts) => {
+    // check if has a session
+    const session_temp = opts.ctx.req.cookies['session'];
+    const session_zod = z.string().safeParse(session_temp);
+    if (!session_zod.success) {
+        throw new TRPCError({
+            message: 'User is not logged in!',
+            code: 'UNAUTHORIZED'
+        })
+    }
+    const session = session_zod.data;
+    const session_from_db = await prisma.session.findUnique({
+        where: {
+            id: session
+        }
+    });
+    if (session_from_db === null ) {
+        throw new TRPCError({
+            message: 'User session does not exists',
+            code: 'UNAUTHORIZED'
+        })
+    }
+    if (session_from_db.expires < new Date()) {
+        await prisma.session.delete({
+            where: {
+                id: session
+            }
+        })
+        throw new TRPCError({
+            message: 'User session has expired',
+            code: 'UNAUTHORIZED'
+        });
+    }
+    
     return opts.next();
 });
 /**
