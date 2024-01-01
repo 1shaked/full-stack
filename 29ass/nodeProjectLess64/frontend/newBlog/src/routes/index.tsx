f3c6fe57a56ac4fe233771bed16c6df266d258ba
1shaked <40497915+1shaked@users.noreply.github.com>
2024-01-01 19:21:09 +0200

    fix login logic

--- 29ass/nodeProjectLess64/frontend/newBlog/src/routes/index.tsx
+++ 29ass/nodeProjectLess64/frontend/newBlog/src/routes/index.tsx
@@ -15,20 +15,25 @@ export function CustomRouter() {
     const set_user_info = useSetAtom(UserInfoAtom)
     const navigate = useNavigate();
     const location = useLocation()
-    // useEffect(() => {
-    //     const user_str = localStorage.getItem(USER_LOCAL_KEY);
-    //     if (user_str === null || user_str.length === 0 && location.pathname !== '/login') return navigate('/sign-up');
-    //     const user_obj_zod = UserTypeZod.safeParse(JSON.parse(user_str)); 
-    //     if (user_obj_zod.success) {
-    //         set_user_info(user_obj_zod.data);
-    //         if (location.pathname === '/sign-up' || location.pathname === '/login') {
-    //             navigate('/blog');
-    //         }
-    //         return ;
-    //     }
-    //     localStorage.removeItem(USER_LOCAL_KEY);
-    //     navigate('/sign-up');
-    // } , []);
+    useEffect(() => {
+        const user_str = localStorage.getItem(USER_LOCAL_KEY);
+        if (user_str === null ){ 
+            if (location.pathname === '/login') return;
+            return navigate('/sign-up');
+        }
+        if (user_str.length === 0 && location.pathname !== '/login') return  navigate('/sign-up');
+        
+        const user_obj_zod = UserTypeZod.safeParse(JSON.parse(user_str)); 
+        if (user_obj_zod.success) {
+            set_user_info(user_obj_zod.data);
+            if (location.pathname === '/sign-up' || location.pathname === '/login') {
+                navigate('/blog');
+            }
+            return ;
+        }
+        localStorage.removeItem(USER_LOCAL_KEY);
+        navigate('/sign-up');
+    } , [location.pathname]);
     return <div>
         <Routes>
             <Route path="/" element={<HomePage />}/>
