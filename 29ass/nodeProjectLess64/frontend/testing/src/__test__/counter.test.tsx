04c0ca1b17ba548928b274abcc5fcaaeb7574935
1shaked <40497915+1shaked@users.noreply.github.com>
2024-01-03 19:43:42 +0200

    count random

--- 29ass/nodeProjectLess64/frontend/testing/src/__test__/counter.test.tsx
+++ 29ass/nodeProjectLess64/frontend/testing/src/__test__/counter.test.tsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-
-// import userEvent from '@testing-library/user-event'
+import { render, screen, fireEvent } from '@testing-library/react'
 
 function Counter () {
     const [count , setCount ] = useState(0);
@@ -14,6 +11,9 @@ function Counter () {
         <button data-testid='increment-button' onClick={() => {
             setCount(count + 1)
         }}>+</button>
+        <button data-testid='decrement-button' onClick={() => {
+            setCount(count - 1)
+        }}>-</button>
     </>
 }
 
@@ -28,7 +28,25 @@ test('testing counter' , () => {
 test('increment by one counter' , () => {
     render(<Counter />);
     const increment_button = screen.getByTestId('increment-button');
-    userEvent.click(increment_button);
+    fireEvent.click(increment_button);
     const text = screen.getByTestId('counter').textContent;
     expect(text).toContain('1');
 });
+
+test('testing multiple clicks' , () => {
+    render(<Counter />);
+
+    let count = 0;
+    
+    // get random number + 1 or -1
+    const change_by = Math.random() < 0.5 ? 1 : -1;
+    
+    const increment_button = screen.getByTestId('increment-button');
+    const decrement_button = screen.getByTestId('decrement-button');
+    if (change_by === 1) fireEvent.click(increment_button);
+    else fireEvent.click(decrement_button);
+    count = count + change_by;
+    const text = screen.getByTestId('counter').textContent;
+    console.log(text)
+    expect(text).toBe(count.toString())
+})
