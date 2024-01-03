1cdf500766f0279b4f0beb78913d1988db2d4e22
1shaked <40497915+1shaked@users.noreply.github.com>
2024-01-03 16:44:30 +0200

    Update Greetings.test.tsx to render the greetings component with the name 'World' and assert the presence of 'Hello, World!' text

--- 29ass/nodeProjectLess64/frontend/newBlog/src/test/__mocks__/Greetings.test.tsx
+++ 29ass/nodeProjectLess64/frontend/newBlog/src/test/__mocks__/Greetings.test.tsx
@@ -1,12 +1,14 @@
 import '@testing-library/jest-dom'
 import { render } from "@testing-library/react"
 import Greeting from '../../components/testing/Greeting'
+import { screen } from '@testing-library/react';
 
 test('demo', () => {
     expect(true).toBe(true)
 })
 
-test("Renders the main page", () => {
-    render(<Greeting name='Hello' />)
-    expect(true).toBeTruthy()
+test("Renders the greetings component", () => {
+    render(<Greeting name='World' />)
+    expect(true).toBeTruthy();
+    expect(screen.getByText('Hello, World!')).toBeInTheDocument();
 })
