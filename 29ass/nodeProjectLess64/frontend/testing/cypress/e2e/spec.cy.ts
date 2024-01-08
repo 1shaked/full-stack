e0929576360cca1e4a99a934eca675c80ad32d68
1shaked <40497915+1shaked@users.noreply.github.com>
2024-01-08 17:18:36 +0200

    added cypress

--- 29ass/nodeProjectLess64/frontend/testing/cypress/e2e/spec.cy.ts
+++ 29ass/nodeProjectLess64/frontend/testing/cypress/e2e/spec.cy.ts
@@ -1,5 +1,19 @@
+/// <reference types="cypress" />
+
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+    cy.log('before each')
+    cy.visit('http://localhost:5173')
+  })
+  it('check if counter exists', () => {
+    cy.get('#counter').should('exist');
+    cy.get('#counter').should('have.text', '0');
+  })
+  it('check if counter works', () => {
+    cy.get('#counter').should('have.text', '0');
+    cy.get('#increment').click();
+    cy.get('#counter').should('have.text', '1');
+    cy.get('#decrement').click();
+    cy.get('#counter').should('have.text', '0');
   })
 })
--- 29ass/nodeProjectLess64/frontend/testing/src/App.tsx
+++ 29ass/nodeProjectLess64/frontend/testing/src/App.tsx
@@ -1,13 +1,19 @@
 
+import { useState } from 'react';
 import './App.css'
-import { Todos } from './components/todos'
+// import { Todos } from './components/todos'
 
 function App() {
-
+  const [count, setCount] = useState(0);
   return (
+
     <>
       <div>
-        <Todos />
+        {/* <Todos /> */}
+        <h1>Counter</h1>
+        <h2 id='counter'>{count}</h2>
+        <button onClick={() => setCount(count + 1)} id='increment'>Increment</button>
+        <button onClick={() => setCount(count - 1)} id='decrement'>Decrement</button>
       </div>
     </>
   )
