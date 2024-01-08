6db5230b6031d63169bc19e22a0283d4b2797b9f
1shaked <40497915+1shaked@users.noreply.github.com>
2024-01-08 19:47:38 +0200

    make the test more modular

--- 29ass/nodeProjectLess64/frontend/testing/cypress/e2e/createSourceTest.cy.ts
+++ 29ass/nodeProjectLess64/frontend/testing/cypress/e2e/createSourceTest.cy.ts
@@ -1,19 +1,27 @@
+import {generateSource} from '../../src/utils/generateSource'
 
+function checkSourceNumbers(n: number) {
+    it(`checking ${n} as a number`, () => {
+        const arr_sources = generateSource(n)
+        cy.get('#source_input').type(n.toString());
+        cy.get('#source_generate_button').should('contain.text', n.toString());
+        cy.get('#source_generate_button').click();
+        for (const source_item of arr_sources) {
+            cy.get(`#source_item_${source_item[0]}_${source_item[1]}`).should('exist'); 
+        }
+    })
+}
 
 describe('Checking the generation of the source', () => {
     beforeEach(() => {
         cy.visit('http://localhost:5173')
     })
-    it('checking 12 as a number', () => {
-        cy.get('#source_input').type('12');
-        cy.get('#source_generate_button').should('contain.text', '12');
-        cy.get('#source_generate_button').click();
-        cy.get('#source_item_1_12').should('exist');
-        cy.get('#source_item_2_6').should('exist');
-        cy.get('#source_item_3_4').should('exist');
-        cy.get('#source_item_4_3').should('exist');
-        cy.get('#source_item_6_2').should('exist');
-    })
+
+    checkSourceNumbers(10);
+    checkSourceNumbers(12);
+    checkSourceNumbers(118);
+    checkSourceNumbers(5);
+    checkSourceNumbers(-5);
     it('checking text as a number', () => {
         cy.get('#source_input').type('12.5');
         cy.get('#source_generate_button').click();
