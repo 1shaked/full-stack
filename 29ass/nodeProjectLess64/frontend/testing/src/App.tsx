d88a37eb6995ffb1155f5bcd07166b1f39f5ba67
1shaked <40497915+1shaked@users.noreply.github.com>
2024-01-08 18:41:38 +0200

    created the source generation

--- 29ass/nodeProjectLess64/frontend/testing/src/App.tsx
+++ 29ass/nodeProjectLess64/frontend/testing/src/App.tsx
@@ -5,6 +5,18 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
+  const [n, setN] = useState(0);
+  const [source, setSource] = useState<[number , number][]>([ ]);
+  // 12 -> [ [1 , 12 ] , [2 , 6], [3 , 4], [4 , 3] , [6 , 2] , ] 
+  function generateSource () {
+    const arr_sources: [number , number][] = []
+    for (let i = 1; i < n; i++) {
+      // check if this is a source
+      if (n % i !== 0) continue;
+      arr_sources.push([i , n / i]);
+    }
+    setSource(arr_sources)
+  }
   return (
 
     <>
@@ -14,6 +26,17 @@ function App() {
         <h2 id='counter'>{count}</h2>
         <button onClick={() => setCount(count + 1)} id='increment'>Increment</button>
         <button onClick={() => setCount(count - 1)} id='decrement'>Decrement</button>
+
+        <hr />
+
+        <input type="number" onChange={(e) => setN(parseInt(e.target.value))} />
+        <button onClick={generateSource}>generate source - {n}</button>
+
+        {source.map((source_item , index) => <div key={index}>
+          {source_item[0]}
+          X
+          {source_item[1]}
+        </div>)}
       </div>
     </>
   )
