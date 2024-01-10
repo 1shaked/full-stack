d6898fc6d6bbaf1a9e7e520eaaf3c338eab0e2b8
1shaked <40497915+1shaked@users.noreply.github.com>
2024-01-10 18:53:39 +0200

    todo with adding number

--- react_native/first/App.tsx
+++ react_native/first/App.tsx
@@ -1,15 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
 
 export default function App() {
   const [count , setCount] = useState(0);
-  
+  const [todos, setTodos] = useState<string[]>([ 'A', 'B', 'C', 'D', 'E', 'F',])
   return (
     <View style={styles.container}>
       <Text style={styles.textBold}>Shaked nf {count}</Text>
-        <Button title='increment' onPress={() => setCount(count + 1)} />
+        <Button title='increment' onPress={() => {
+          setCount(count + 1);
+          setTodos((prev) => {
+            return [...prev, count.toString()]
+          })
+        }} />
         <Button title='decrement' onPress={() => setCount(count - 1)} />
+        {/* {todos.map((todo, index) => <Text key={index}>
+          {todo}
+
+         
+        </Text>)} */}
+
+        <FlatList data={todos} 
+          renderItem={(item) => <Text> {item.item}</Text>}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,12 +30,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center',
-
-
+    // justifyContent: 'center',
   },
   textBold : {
     fontWeight: 'bold',
