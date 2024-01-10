39b97809e21028622a7a9ee43d978519148ad50a
1shaked <40497915+1shaked@users.noreply.github.com>
2024-01-10 19:52:15 +0200

    show images

--- react_native/first/App.tsx
+++ react_native/first/App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, FlatList, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList, TextInput, Image } from 'react-native';
 
 export default function App() {
   const [count , setCount] = useState(0);
   const [todos, setTodos] = useState<string[]>([ 'A', 'B', 'C', 'D', 'E', 'F',])
-  const [newTodo, setNewTodo] = useState('')
+  const [newTodo, setNewTodo] = useState('');
+
+  const [images, setImages] = useState<string[]>([
+    'https://image.lexica.art/full_webp/665f425f-31f8-4d19-a10c-20d133252959'
+  ])
   return (
     <View style={styles.container}>
       <Text style={styles.textBold}>Shaked nf {count}</Text>
@@ -30,8 +34,21 @@ export default function App() {
             return [...prev, newTodo]
           })
         }}></Button>
+
+        <FlatList 
+        data={images}
+        renderItem={(item) => <Image source={{ uri: item.item}}/>}
+        />
+        <Image 
+        style={{
+          width: 100,
+          height: 100,
+        }}
+        source={{ uri: 'https://image.lexica.art/full_webp/54ea9d47-3dc2-40a9-905f-c0ae51359479' }} />
         {/* {todos.map((todo, index) => <Text key={index}>
           {todo}
+
+      
 
          
         </Text>)} */}
