ea3b2979ef5878f5770cdf242214d3ee1b9b42cd
1shaked <40497915+1shaked@users.noreply.github.com>
2024-01-10 19:07:34 +0200

    todo list with adding options

--- react_native/first/App.tsx
+++ react_native/first/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList, TextInput } from 'react-native';
 
 export default function App() {
   const [count , setCount] = useState(0);
   const [todos, setTodos] = useState<string[]>([ 'A', 'B', 'C', 'D', 'E', 'F',])
+  const [newTodo, setNewTodo] = useState('')
   return (
     <View style={styles.container}>
       <Text style={styles.textBold}>Shaked nf {count}</Text>
@@ -15,14 +16,29 @@ export default function App() {
           })
         }} />
         <Button title='decrement' onPress={() => setCount(count - 1)} />
+
+        <TextInput style={{
+          height: 40,
+          borderColor: '#ccc',
+          borderWidth: 1,
+          width: 150
+        }} placeholder='placeholder'
+        onChangeText={(e) => setNewTodo(e)} />
+        <Text>{newTodo}</Text>
+        <Button title='add todo' onPress={() => {
+          setTodos((prev) => {
+            return [...prev, newTodo]
+          })
+        }}></Button>
         {/* {todos.map((todo, index) => <Text key={index}>
           {todo}
 
          
         </Text>)} */}
-
-        <FlatList data={todos} 
-          renderItem={(item) => <Text> {item.item}</Text>}/>
+          <FlatList showsVerticalScrollIndicator={false} data={todos} 
+            renderItem={(item) => <Text> {item.item}</Text>}/>
+        {/* <ScrollView>
+        </ScrollView> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -30,10 +46,10 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
+    flex: 1,
     backgroundColor: 'white',
     alignItems: 'center',
-    // justifyContent: 'center',
+    justifyContent: 'center',
   },
   textBold : {
     fontWeight: 'bold',
