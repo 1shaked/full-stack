a56364e06fd35077d64aa226250ecaf42b57e281
1shaked <40497915+1shaked@users.noreply.github.com>
2024-01-15 19:03:01 +0200

    added socket io

--- node-ws/backend/server/app.ts
+++ node-ws/backend/server/app.ts
@@ -2,33 +2,45 @@ import express from 'express';
 import http from 'http';
 import WebSocket from 'ws';
 import cors from 'cors'
+import { Server } from 'socket.io';
+
 const app = express();
+
 app.use(cors({
     origin: ['*']
 }));
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-app.get('/example', (req, res) => {
-    res.send({
-        message: 'wow',
-        title: 'no title'
-    });
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    }
 });
+// const wss = new WebSocket.Server({ server });
+// app.get('/example', (req, res) => {
+//     res.send({
+//         message: 'wow',
+//         title: 'no title'
+//     });
+// });
 
 // WebSocket server event handlers
-wss.on('connection', (ws) => {
-    console.log('WebSocket connection opened');
+// wss.on('connection', (ws) => {
+//     console.log('WebSocket connection opened', ws.rawListeners);
 
-    ws.on('message', (message) => {
-        console.log(`Received message: ${message}`);
-        // Handle WebSocket messages here
-    });
+//     ws.on('message', (message) => {
+//         console.log(`Received message: ${message}`, message, message.toString('ascii'));
+//         // Handle WebSocket messages here
+//     });
 
-    ws.on('close', () => {
-        console.log('WebSocket connection closed');
-    });
+//     ws.on('close', () => {
+//         console.log('WebSocket connection closed');
+//     });
+// });
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
 });
-
+  
 const PORT = process?.env?.PORT ?? 8080;
 
 server.listen(PORT, () => {
